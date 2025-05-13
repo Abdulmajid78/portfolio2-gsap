46e86fafd15ff964d3a1f1adff5b82db6a115da5
Abdulmajid78 <jackodilov@gmail.com>
2025-05-13 16:29:09 +0500

    script added

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,9 @@ import {Footer} from "@/components/footer"
 import {DynamicBackground} from "@/components/dynamic-background"
 import {ScrollProgress} from "@/components/scroll-progress"
 import type React from "react"
+import Script from 'next/script';
+import {Navigation} from "@/components/navigation";
+
 
 const inter = Inter({subsets: ["latin"]})
 
@@ -71,19 +74,25 @@ export default function RootLayout({children,}: { children: React.ReactNode }) {
             {/*></script>*/}
 
             {/*google tag manager*/}
-            <script>(function(w,d,s,l,i){w[l] = w[l] || [];w[l].push({'gtm.start':
-                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            <Script id="gtm-script" strategy="afterInteractive">
+                {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;
+            j.src='https://www.googletagmanager.com/gtm.js?id=GTM-WQKC9ZJ5'+dl;
+            f.parentNode.insertBefore(j,f);
             })(window,document,'script','dataLayer','GTM-WQKC9ZJ5');
-            </script>
+          `}
+            </Script>
 
-            <!-- Google Tag Manager (noscript) -->
             <noscript>
-                <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WQKC9ZJ5"
-                        height="0" width="0" style="display:none;visibility:hidden"></iframe>
+                <iframe
+                    src="https://www.googletagmanager.com/ns.html?id=GTM-WQKC9ZJ5"
+                    height="0"
+                    width="0"
+                    style={{display: 'none', visibility: 'hidden'}}
+                ></iframe>
             </noscript>
-            <!-- End Google Tag Manager (noscript) -->
 
             {/*end google tag manager*/}
 
@@ -116,5 +125,3 @@ export default function RootLayout({children,}: { children: React.ReactNode }) {
 }
 
 
-import './globals.css'
-import {Navigation} from "@/components/navigation";
